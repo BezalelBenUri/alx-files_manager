983ebb661c2488f77dbb064a410c6c7b290ebdc9
BezalelBenUri <diamondinyang@gmail.com>
2024-03-12 09:41:16 -0700

    creating and authenticating user

--- server.js
+++ server.js
@@ -1,11 +1,9 @@
 import express from 'express';
-import startServer from './libs/boot';
+
 import injectRoutes from './routes';
-import injectMiddlewares from './libs/middlewares';
 
 const server = express();
 
-injectMiddlewares(server);
 injectRoutes(server);
 startServer(server);
 
